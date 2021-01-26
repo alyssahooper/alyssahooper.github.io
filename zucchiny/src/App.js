4fcf49572da5d882a2e82e9b53b8292aa707d53f
Alyssa Hooper <nesteralyssa@gmail.com>
2021-01-25 19:29:46 -0800

    Updates font family

--- zucchiny/src/App.js
+++ zucchiny/src/App.js
@@ -4,7 +4,7 @@ import Countdown from './components/Countdown'
 function App() {
   return (
     <div className="App">
-      <span>Coming soon</span>
+      <span className="App__Title"><i>It's beginning to look a lot like</i> <strong>cocktails</strong></span>
       <Countdown />
       <Footer />
     </div>
--- zucchiny/src/components/Countdown.js
+++ zucchiny/src/components/Countdown.js
@@ -1,7 +1,7 @@
 function Countdown(){
   return(
-    <div>
-    count placeholder
+    <div className="Countdown">
+      count placeholder
     </div>
   );
 }
